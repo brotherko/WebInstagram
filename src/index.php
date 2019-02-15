428c20b21cbfd65c36de3204e2b966267c49f786
unknown <ko19950314@gmail.com>
2019-02-15 19:11:39 +0800

    image validation

--- src/index.php
+++ src/index.php
@@ -1,36 +1,39 @@
-  <?php
-    include 'lib/base.php';
-    include 'lib/image.class.php';
-    if($current_user->is_loggedin()){
-      if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        // print_r($_POST); print_r($_FILES);
-        if(isset($_POST['logout'])){
-          $current_user->logout();
-        }
-        if(isset($_POST['upload']) && isset($_FILES['file']) && $_FILES['file']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['file']['tmp_name'])){
-          $image_svc = new ImageService();
-          $image_svc->upload($_FILES['file']);
+<?php
+  include 'lib/base.php';
+  include 'lib/image.class.php';
+  if($current_user->is_loggedin()){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      if(isset($_POST['logout'])){
+        $current_user->logout();
+      }
+      if(isset($_POST['upload']) && isset($_FILES['file']) && $_FILES['file']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['file']['tmp_name'])){
+        $image_svc = new ImageService();
+        if(!$image_svc->is_valid_image($_FILES['file'])){
+         printf("This is not valid image");
+        } else {
+          if($image_svc->upload($_FILES['file'], $current_user->id)){
+            printf("Image successfully uploaded");
+          }else{
+            printf("Error while uploading image");
+          }
         }
       }
     }
-$image = new Imagick();
-$image->newImage(1, 1, new ImagickPixel('#ffffff'));
-$image->setImageFormat('png');
-$pngData = $image->getImagesBlob();
-echo strpos($pngData, "\x89PNG\r\n\x1a\n") === 0 ? 'Ok' : 'Failed'; 
-  ?>
-  <form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-  <h2>Member Area</h2>
-  <?php
-    if($current_user->is_loggedin()){
-    echo 'You have been logged in as '.$current_user->username;
-  ?>
-    <button type="submit" name="logout">Logout</button><br />
+  }
+?>
+<form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<h2>Member Area</h2>
+<?php
+  if($current_user->is_loggedin()){
+  echo 'You have been logged in as '.$current_user->username;
+?>
+  <button type="submit" name="logout">Logout</button><br />
 
-    <h2>Upload Image</h2>
-    <input name="file" type="file">
-    <button type="submit" name="upload">Upload</button>
-  <?php } else { ?>
-  <a href="login.php">login</a>
-  <?php } ?>
-  </form>
+  <h2>Upload Image</h2>
+  <input name="file" type="file"> <br>
+  <input type="radio" name="visbility" value="1"> Public <input type="radio" name="visbility" value="0"> Private<br>
+  <button type="submit" name="upload">Upload</button>
+<?php } else { ?>
+<a href="login.php">login</a>
+<?php } ?>
+</form>
--- src/lib/image.class.php
+++ src/lib/image.class.php
@@ -4,9 +4,10 @@ require_once __DIR__.'/../../vendor/autoload.php';
 class ImageService {
   private $dal = null;
   private $s3 = null;
+  private $bucket = null;
   const VALID_EXTS = array("jpg", "png", "gif");
   const VALID_TYPES = array("image/jpeg", "image/gif", "image/png");
-
+  const VALID_MINETYPE = array("image/x-png", "image/x-gif", "image/x-jpeg");
   function __construct(){
     $this->dal = ($dal) ? $dal : new DAL();
     $this->s3 = new Aws\S3\S3Client([
@@ -17,30 +18,56 @@ class ImageService {
   }
 
   private function is_valid_image_exts($file){
-    return in_array(substr($file[name], -3), VALID_EXTS);
+    $cond = in_array(substr($file['name'], -3), self::VALID_EXTS);
+    return $cond;
   }
 
   private function is_valid_image_types($file){
-    return in_array($file[types], VALID_TYPES);
+    $cond = in_array($file['type'], self::VALID_TYPES);
+    return $cond;
+  }
+
+  private function is_valid_image_content($file){
+    $imagick = new Imagick($file['tmp_name']);
+    try{
+      $image_info = $imagick->identifyImage(false);
+    } catch (Exception $e){
+      return false;
+    }
+    $cond = in_array($image_info['mimetype'], self::VALID_MINETYPE);
+    return $cond;
   }
 
   public function is_valid_image($file){
-    return is_valid_image_exts($file) && is_valid_image_types($file);
+    return $this->is_valid_image_exts($file)
+        && $this->is_valid_image_types($file)
+        && $this->is_valid_image_content($file);
   }
 
-  public function upload($file){
+  public function upload($file, $user_id){
     try{
       print_r($file);
-      $upload = $s3->putObject([
-        'Bucket' => $bucket,
+      $upload = $this->s3->putObject([
+        'Bucket' => $this->bucket,
         'Key' => time().".png",
         'SourceFile' => $file['tmp_name'],
         'ACL' => 'public-read',
-        'ContentType' => $file['type'],
+        'ContentType' => $file['type']
       ]);
     } catch(Exception $e){
       print($e);
+      return false;
     }
+    try{
+      $this->dal->add_image([$upload_result['ObjectURL'], NOW, $user_id, $_POST['visbility']]);
+    } catch(PDOException $e){
+      printf("DB Error:%s", $e);
+      return false;
+    }
+    $this->is_valid_image_content($file);
+    return true;
   }
+  
+  
 }
 ?>
